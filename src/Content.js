46d6fcec15188ede55f43edee1c1e5ff7e7dc673
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2022-11-04 21:42:50 -0400

    Click Event

--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,26 @@ const Content = () => {
 
   }
 
+  const handleClick = () => {
+    console.log("You clicked it")
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`)
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
+
   return (
     <main>
-      <p>
+      <p onDoubleClick={handleClick}>
         Hello {handleNameChange()}!
       </p>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClick2("Adi")}>Click It</button>
+      <button onClick={(e) => handleClick3(e)}>Click It</button>
     </main>
   )
 }
